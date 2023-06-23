00c70147e59dcf7ef3d3554d436e6136ec395f91
fandi-ahmad <fandi4160@gmail.com>
2023-06-23 20:32:23 +0800

    add filter for upload image

---
+++ middleware/filterImage.js
@@ -0,0 +1,20 @@
+const { diskStorage } = require('multer')
+
+const fileStorage = diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().getTime() + '-' + file.originalname)
+    }
+})
+
+const filterFormat = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true)
+    } else {
+        cb(null,  false)
+    }
+}
+
+module.exports = { fileStorage, filterFormat }
